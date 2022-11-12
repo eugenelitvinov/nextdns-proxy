67a7651a70fb0084ba5cc6e2a6646f1ed4868195
Eugene Litvinov <eugene.litvinov@gmail.com>
2022-11-12 17:08:56 +0100

    Allow multiple forwarders, amend other defaults

--- nextdns-proxy/run.sh
+++ nextdns-proxy/run.sh
@@ -3,8 +3,7 @@
 NEXTDNS_MAX_TTL=${NEXTDNS_MAX_TTL:-15}
 NEXTDNS_CACHE_SIZE=${NEXTDNS_CACHE_SIZE:-10MB}
 NEXTDNS_CONFIG_ID=$NEXTDNS_CONFIG
-NEXTDNS_ARGUMENTS="-listen :53 -report-client-info -log-queries -cache-size=${NEXTDNS_CACHE_SIZE} -max-ttl=${NEXTDNS_MAX_TTL}s"
-
+NEXTDNS_ARGUMENTS="-listen :53 -report-client-info -log-queries -cache-size=${NEXTDNS_CACHE_SIZE} -max-ttl=${NEXTDNS_MAX_TTL}s -use-hosts=false -mdns=disabled -bogus-priv=false"
 echo "Parsing configuration"
 
 while IFS='=' read -r -d '' n v; do
@@ -23,6 +22,18 @@ if [ -n "$NEXTDNS_FORWARDING_DOMAIN" ]; then
     if [ -n "$NEXTDNS_FORWARDING_DNSIP" ]; then
         NEXTDNS_ARGUMENTS+="  -forwarder $NEXTDNS_FORWARDING_DOMAIN=$NEXTDNS_FORWARDING_DNSIP"
     fi
+fi
+
+while IFS='=' read -r -d '' n v; do
+    if [[ "$n" = "NEXTDNS_FORWARDING_"* ]]; then
+        echo " => Found custom forwarding config: $n => $v"
+        NEXTDNS_ARGUMENTS+=" -forwarder $v"
+    fi
+done < <(env -0)
+
+if [ -n "$NEXTDNS_DISCOVERY_DNS" ]; then
+    NEXTDNS_ARGUMENTS+=" -discovery-dns $NEXTDNS_DISCOVERY_DNS"
+    echo " => Found discovery DNS: $NEXTDNS_DISCOVERY_DNS"
 fi
 
 echo "Running nextdns with arguments: $NEXTDNS_ARGUMENTS"
